7efc4a7be7fb29ef8bb3b927f3a7a6b847eb97d2
cen <cen.gao@outlook.com>
2023-07-27 16:09:10 +0100

    tdd1

--- java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
@@ -3,17 +3,37 @@ package com.db.grad.javaapi.service;
 import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.repository.DogsRepository;
 
+import java.util.List;
+
 public class DogHandler {
     private DogsRepository itsDogsRepo;
+
     public DogHandler(DogsRepository repo) {
         itsDogsRepo = repo;
     }
+
+
 
     public long addDog(Dog theDog) {
         return itsDogsRepo.save(theDog);
     }
 
+
     public long getNoOfDogs() {
         return itsDogsRepo.count();
+    }
+
+
+    public Dog getDogByName(String dogName) {
+        Dog targetDog = new Dog();
+        targetDog.setName(dogName);
+        List<Dog> dogs = itsDogsRepo.findByName(targetDog);
+        Dog result = null;
+
+        if (dogs.size() == 1)
+            result = dogs.get(0);
+
+        return result;
+
     }
 }
--- java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
@@ -2,11 +2,17 @@ package com.db.grad.javaapi.service;
 
 import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.repository.DogsRepository;
+
 import com.db.grad.javaapi.repository.DogsRepositoryStub;
 import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class DogsHandlerTest {
+
     private DogsRepository itsDogRepo = new DogsRepositoryStub();
     @BeforeEach
     public void makeSureRepoIsEmpty(){
@@ -28,4 +34,77 @@ public class DogsHandlerTest {
         // assert
         assertEquals(expectedResult, actualResult);
     }
+
+    @Test
+    public  void    add_several_dogs_return_number_of_dogs_match_number_added(){
+        // arrange
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        cut.addDog(theDog);
+        Dog dog1 = new Dog();
+        dog1.setName("Wang Cai");
+        cut.addDog(dog1);
+        Dog dog2 = new Dog();
+        dog2.setName("Hu Zi");
+        cut.addDog(dog2);
+
+
+        int expectedResult = 3;
+
+        // act
+        long actualResult = cut.getNoOfDogs();
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void test_getDogsByName_if_valid(){
+        DogHandler dhl = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        dhl.addDog(theDog);
+        Dog dog1 = new Dog();
+        dog1.setName("Wang Cai");
+        dhl.addDog(dog1);
+        Dog dog2 = new Dog();
+        dog2.setName("Hu Zi");
+        dhl.addDog(dog2);
+
+        ArrayList<Dog> expected = new ArrayList<>();
+        Mockito.when(itsDogRepo.findByName(Mockito.any())).thenReturn(expected);
+        // act
+
+        Dog actDog = dhl.getDogByName("Hua Zi");
+
+        // assert
+        assertNull(actDog);
+    }
+
+    public void find_dog_by_name_returns_null_because_many_dogs_with_same_name() {
+        DogHandler cut = new DogHandler(itsDogRepo);
+        // arrange
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        cut.addDog(theDog);
+        theDog = new Dog();
+        theDog.setName("Bruno");
+        cut.addDog(theDog);
+        Dog expectedDog = theDog;
+        String dogToFind = "Bruno";
+        theDog = new Dog();
+        theDog.setName("Penny");
+        cut.addDog(theDog);
+        ArrayList<Dog> expectedList = new ArrayList<>();
+        Mockito.when(itsDogRepo.findByName(Mockito.any())).thenReturn(expectedList);
+
+        // act
+        Dog actualResult = cut.getDogByName(dogToFind);
+
+        // assert
+        assertNull(actualResult);
+    }
+    }
+
 }
