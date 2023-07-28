72787d5c808bc1da5d1707986f5b0e4668eb208c
unknown <cen.gao@outlook.com>
2023-07-28 14:49:12 +0100

    QL7

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,38 +1,20 @@
 package com.example.superheroes;
 
-import com.example.superheroes.practice.Calculator;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import com.example.superheroes.model.Hero;
+import com.example.superheroes.repository.HeroRepository;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SuperheroesApplication implements CommandLineRunner {
+import java.util.List;
 
-	@Autowired
-	Calculator calc;
+@SpringBootApplication
+public class SuperheroesApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
-	}
-
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hallo World");
-		int result = calc.addition(3,4);
-		System.out.println(result);
-	}
-
-	@PreDestroy
-	public void destroy(){
-		System.out.println("Calculator will be destroyed");
-	}
-
-	@PostConstruct
-	public void intit(){
-		System.out.println("Calculator has been destroyed");
 	}
 
 
