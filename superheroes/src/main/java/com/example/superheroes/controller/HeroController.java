8abebcba8f0edda4d9e33a5dfd8b7aeefe39c3e8
Liviu <liviurosu08@gmail.com>
2023-07-28 14:35:39 +0100

    qlc 6 finished

--- superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
+++ superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
@@ -4,6 +4,7 @@ import com.example.superheroes.model.Hero;
 import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +16,12 @@ public class HeroController {
 
     @GetMapping("/heroes")
     public List<Hero> getAllHeroes(){
+
         return heroService.getAllHeroes();
     }
+    @GetMapping("/heroes/{letter}")
+    public List<Hero> getHeroesStartingWithLetter(@PathVariable String letter) {
+        return heroService.getHeroesStartingWithLetter(letter);
+    }
+
 }
