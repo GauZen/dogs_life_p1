420167e07b1f9e31e4a133c96a7373b3a1a34c9a
Liviu <liviurosu08@gmail.com>
2023-07-28 10:37:49 +0100

    "liviu ql1-2 finished"

--- superheroes/src/main/java/com/example/superheroes/practice/Calculator.java
+++ superheroes/src/main/java/com/example/superheroes/practice/Calculator.java
@@ -1,10 +1,23 @@
 package com.example.superheroes.practice;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jdk.jfr.Percentage;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Calculator {
     public int addition(int num1, int num2){
         return num1+num2;
+    }
+    @PostConstruct
+    public void init(){
+        System.out.println("Calculator has been created!!!1");
+
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("calculator will be destroyed :(((");
     }
 }
