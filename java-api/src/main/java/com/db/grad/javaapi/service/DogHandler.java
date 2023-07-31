76a4e8641295db4901fc98582d41eefaa9881d37
unknown <cen.gao@outlook.com>
2023-07-31 08:42:05 +0100

    test imported

--- java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
@@ -1,49 +1,24 @@
 package com.db.grad.javaapi.service;
+
 import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.repository.DogsRepository;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
 
-public class DogHandler
-{
-    private DogsRepository itsDogsRepo;
-    public DogHandler(DogsRepository repo)
-    {
-        itsDogsRepo = repo;
-    }
-    public long addDog(Dog theDog)
-    {
-        return itsDogsRepo.save( theDog );
-    }
-    public long getNoOfDogs()
-    {
-        return itsDogsRepo.count();
-    }
-    public boolean removeDog(long uniqueId)
-    {
-        boolean result = false;
-        Dog theDog = itsDogsRepo.findById(uniqueId);
-        if(theDog != null)
-        {
-            result = itsDogsRepo.delete(theDog);
-        }
-        return  result;
-    }
-    public Dog getDogById(long uniqueId)
-    {
-        return itsDogsRepo.findById(uniqueId);
-    }
 
-    public Dog getDogByName(String dogsName )
-    {
-        Dog dogToFind = new Dog();
-        dogToFind.setName(dogsName);
-        List<Dog> dogs = itsDogsRepo.findByName(dogToFind);
-        Dog result = null;
+@Service
+public class DogHandler{
+        private DogsRepository itsDogsRepo;
 
-        if( dogs.size() == 1)
-            result = dogs.get(0);
+            @InjectMocks
+        private DogHandler cut;
 
-        return result;
-    }
+                @Test
+        public  void    add_a_dog_return_number_of_dogs_in_repo_is_one()
+                {
+                            // arrange
+                            Dog theDog = new Dog();
+                                    theDog.setName("Bruno");
+                                            Mockito.when(itsDogsRepo.save(theDog)).thenReturn();
+                }
 }
