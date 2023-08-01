3a6d9c1203fdd50db7550175baeb743053bdc238
unknown <cen.gao@outlook.com>
2023-08-01 14:13:19 +0100

    start

---
+++ java-api/dogs-app-ui/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import DogDetail from './DogDetail';
+
+function App() {
+  return (
+
+    <div>
+      <p id="blue-alias">ID:1</p>
+      <p className="green-class">Name: WongWong</p>
+      <p className="green-class">Age: 3</p>
+      <p className="green-class">OwnerName: Bruce</p>
+    </div>
+  );
+}
+
+export default App;
